3b72d2fa0cf980c1d5b1ba4bdffce32a144376db
Michael A. Cook <mcook0775@gmail.com>
2021-05-03 21:59:55 -0400

    More progress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react"
-import { Button, Container, Dropdown, Grid, Progress } from "semantic-ui-react"
+import {
+  Button,
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Progress,
+} from "semantic-ui-react"
 import GameSpaceCollection from "./lib/GameSpaceCollection"
 import GameSoundCollection from "./lib/GameSoundCollection"
 import GameSpace from "./lib/GameSpace"
@@ -13,15 +21,17 @@ class App extends Component {
     this.state = {
       game: new Game(2),
       iteration: -1,
+      level: 0,
+      trials: 20,
       currentVisualScore: 0,
       currentAudioScore: 0,
       currentMissedVisualMatches: 0,
       currentMissedAudioMatches: 0,
       currentGameSpaces: [],
-      gameSoundCollection: [],
+      currentGameSounds: [],
       gameSpaceTrialHistory: [],
       gameSoundTrialHistory: [],
-      trialProgress: -4.35,
+      trialProgress: 0,
       0: null,
       1: null,
       2: null,
@@ -32,6 +42,14 @@ class App extends Component {
       7: null,
       8: null,
     }
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.start)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.start)
   }
 
   incrementNBack = () => {
@@ -60,12 +78,24 @@ class App extends Component {
     })
   }
 
-  componentDidMount() {
-    document.addEventListener("keydown", this.start)
+  incrementProgressBar = () => {
+    this.setState((prevState) => ({
+      trialProgress: prevState.trialProgress + 4,
+    }))
   }
 
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.start)
+  incrementLevel = () => {
+    this.setState((prevState) => ({
+      level: prevState.level + 1,
+    }))
+  }
+
+  decrementTrials = () => {
+    if (this.state.trials > 0) {
+      this.setState((prevState) => ({
+        trials: prevState.trials - 1,
+      }))
+    }
   }
 
   /**
@@ -78,7 +108,7 @@ class App extends Component {
       const game = this.state.game
       game.active = false
       const currentGameSpaces = this.state.currentGameSpaces
-      const gameSoundCollection = this.state.gameSoundCollection
+      const currentGameSounds = this.state.currentGameSounds
       const gameSpaceTrialHistory = this.state.gameSpaceTrialHistory
       const gameSoundTrialHistory = this.state.gameSoundTrialHistory
       this.setState(
@@ -90,18 +120,19 @@ class App extends Component {
           ],
           gameSoundTrialHistory: [
             ...gameSoundTrialHistory,
-            new GameSoundCollection(gameSoundCollection),
+            new GameSoundCollection(currentGameSounds),
           ],
         },
         () => {
           this.calculateCurrentScores()
         }
       )
-    }, 2000)
+    }, 3000)
   }
 
   /**
    * Calculate visual and audio scores from most recent game trial and set to state
+   * Increment or decrement nBack based on calculated scores
    */
   calculateCurrentScores = () => {
     const nBack = this.state.game.nBack
@@ -111,24 +142,30 @@ class App extends Component {
     const possibleVisualMatches = spaceCollection.possibleMatches(nBack)
     const possibleAudioMatches = soundCollection.possibleMatches(nBack)
 
-    const visualScore = spaceCollection.matches(nBack)
-    const audioScore = soundCollection.matches(nBack)
+    const visualScore = Math.floor(
+      (spaceCollection.matches(nBack) / possibleVisualMatches) * 100
+    )
+    const audioScore = Math.floor(
+      (soundCollection.matches(nBack) / possibleAudioMatches) * 100
+    )
 
     const missedVisualMatches = spaceCollection.missedMatches(nBack)
     const missedAudioMatches = soundCollection.missedMatches(nBack)
 
     this.setState({
-      currentVisualScore:
-        visualScore > 0
-          ? `${Math.floor((visualScore / possibleVisualMatches) * 100)}%`
-          : 0,
-      currentAudioScore:
-        audioScore > 0
-          ? `${Math.floor((audioScore / possibleAudioMatches) * 100)}%`
-          : 0,
+      currentVisualScore: visualScore > 0 ? `${visualScore}%` : 0,
+      currentAudioScore: audioScore > 0 ? `${audioScore}%` : 0,
       currentMissedVisualMatches: missedVisualMatches,
       currentMissedAudioMatches: missedAudioMatches,
     })
+
+    if (visualScore >= 70 && audioScore >= 70) {
+      this.incrementNBack()
+    }
+
+    if (visualScore < 40 && audioScore < 40 && this.state.game.nBack > 2) {
+      this.decrementNBack()
+    }
   }
 
   /**
@@ -142,14 +179,14 @@ class App extends Component {
     const sound = GameSound.createRandomGameSound()
     const key = space.key
     const currentGameSpaces = this.state.currentGameSpaces
-    const gameSoundCollection = this.state.gameSoundCollection
-    gameSoundCollection.push(sound)
+    const currentGameSounds = this.state.currentGameSounds
+    currentGameSounds.push(sound)
     currentGameSpaces.push(space)
     this.setState(
       {
         [key]: space,
         currentGameSpaces: currentGameSpaces,
-        gameSoundCollection: gameSoundCollection,
+        currentGameSounds: currentGameSounds,
       },
       () => {
         sound.play()
@@ -160,9 +197,11 @@ class App extends Component {
         {
           [key]: null,
         },
-        () => true
+        () => {
+          this.incrementProgressBar()
+        }
       )
-    }, 1000)
+    }, 1200)
   }
 
   /**
@@ -174,11 +213,10 @@ class App extends Component {
       this.setState(
         (prevState) => ({
           iteration: prevState.iteration + 1,
-          trialProgress: prevState.trialProgress + 4.35,
         }),
         this.runTrialIteration
       )
-      if (this.state.iteration === 23) {
+      if (this.state.iteration === 24) {
         clearInterval(id)
         this.completeGameTrial()
       }
@@ -190,18 +228,28 @@ class App extends Component {
    * @param {Object} e - SyntheticEvent
    */
   start = (e) => {
-    const game = this.state.game
-    game.active = true
-    this.setState(
-      {
-        game: game,
-      },
-      () => {
-        if (e.key === " " || e.key === 32) {
+    if (e.key === " " || e.key === 32) {
+      const game = this.state.game
+      game.active = true
+      this.setState(
+        {
+          game: game,
+          iteration: -1,
+          currentVisualScore: 0,
+          currentAudioScore: 0,
+          currentMissedVisualMatches: 0,
+          currentMissedAudioMatches: 0,
+          currentGameSpaces: [],
+          currentGameSounds: [],
+          trialProgress: 0,
+        },
+        () => {
+          this.incrementLevel()
+          this.decrementTrials()
           this.runGameTrial()
         }
-      }
-    )
+      )
+    }
   }
 
   assertSpaceMatch = () => {
@@ -213,28 +261,197 @@ class App extends Component {
   }
 
   assertSoundMatch = () => {
-    const gameSoundCollection = this.state.gameSoundCollection
-    gameSoundCollection[gameSoundCollection.length - 1].matchAsserted = true
+    const currentGameSounds = this.state.currentGameSounds
+    currentGameSounds[currentGameSounds.length - 1].matchAsserted = true
     this.setState({
-      gameSoundCollection: gameSoundCollection,
+      currentGameSounds: currentGameSounds,
     })
   }
 
   render() {
     return (
       <Container>
-        <Grid columns={3} style={{ width: "600px", margin: "0 auto" }}>
-          <Grid.Column textAlign="left">
-            <p>Visual Score: {this.state.currentVisualScore}</p>
-            <p>Audio Score: {this.state.currentAudioScore}</p>
-          </Grid.Column>
-          <Grid.Column textAlign="left">
+        <span>
+          <img src="logo.png" alt="logo"></img>
+          <Header
+            style={{
+              marginTop: "35px",
+              display: "inline-block",
+            }}
+            as="h1"
+          >
+            Dual N-Back
+          </Header>
+        </span>
+        <Grid relaxed>
+          <Grid.Column
+            width={3}
+            textAlign="left"
+            style={{
+              marginTop: "27px",
+              backgroundColor: "#E5E5E5",
+              borderRadius: "3px",
+              padding: "28px",
+            }}
+          >
             <p>
-              Missed Visual Matches: {this.state.currentMissedVisualMatches}
+              Visual Score:{" "}
+              <span style={{ fontWeight: "bold" }}>
+                {this.state.currentVisualScore}
+              </span>
             </p>
-            <p>Missed Audio Matches: {this.state.currentMissedAudioMatches}</p>
+            <p>
+              Audio Score:{" "}
+              <span style={{ fontWeight: "bold" }}>
+                {this.state.currentAudioScore}
+              </span>
+            </p>
+            <p>
+              Missed Visual Matches:{" "}
+              <span style={{ fontWeight: "bold" }}>
+                {this.state.currentMissedVisualMatches}
+              </span>
+            </p>
+            <p>
+              Missed Audio Matches:{" "}
+              <span style={{ fontWeight: "bold" }}>
+                {this.state.currentMissedAudioMatches}
+              </span>
+            </p>
+            <Divider />
+            <p>What Is Dual N-Back?</p>
+            <p>How To Play</p>
           </Grid.Column>
-          <Grid.Column textAlign="right">
+          <Grid.Column width={9}>
+            <Grid id="grid" columns="3" celled style={{ borderRadius: "3px" }}>
+              <Grid.Row style={{ height: "175px" }}>
+                <Grid.Column>
+                  <div
+                    style={{ height: "100%" }}
+                    className={this.state[0] ? "visible-space" : null}
+                    key={0}
+                  ></div>
+                </Grid.Column>
+                <Grid.Column>
+                  <div
+                    style={{ height: "100%" }}
+                    className={this.state[1] ? "visible-space" : null}
+                    key={1}
+                  ></div>
+                </Grid.Column>
+                <Grid.Column>
+                  <div
+                    style={{ height: "100%" }}
+                    className={this.state[2] ? "visible-space" : null}
+                    key={2}
+                  ></div>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row style={{ height: "175px" }}>
+                <Grid.Column>
+                  <div
+                    style={{ height: "100%" }}
+                    className={this.state[3] ? "visible-space" : null}
+                    key={3}
+                  ></div>
+                </Grid.Column>
+                <Grid.Column>
+                  <div
+                    style={{ height: "100%" }}
+                    className={this.state[4] ? "visible-space" : null}
+                    key={4}
+                  ></div>
+                </Grid.Column>
+                <Grid.Column>
+                  <div
+                    style={{ height: "100%" }}
+                    className={this.state[5] ? "visible-space" : null}
+                    key={5}
+                  ></div>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row style={{ height: "175px" }}>
+                <Grid.Column>
+                  <div
+                    style={{ height: "100%" }}
+                    className={this.state[6] ? "visible-space" : null}
+                    key={6}
+                  ></div>
+                </Grid.Column>
+                <Grid.Column>
+                  <div
+                    style={{ height: "100%" }}
+                    className={this.state[7] ? "visible-space" : null}
+                    key={7}
+                  ></div>
+                </Grid.Column>
+                <Grid.Column>
+                  <div
+                    style={{ height: "100%" }}
+                    className={this.state[8] ? "visible-space" : null}
+                    key={8}
+                  ></div>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+            <Grid columns={3} centered>
+              <Grid.Column>
+                <Button
+                  textAlign="center"
+                  onClick={this.assertSoundMatch}
+                  fluid
+                  size="big"
+                >
+                  Audio Match
+                </Button>
+              </Grid.Column>
+              <Grid.Column>
+                <Button
+                  textAlign="center"
+                  onClick={() => {
+                    this.assertSoundMatch()
+                    this.assertSpaceMatch()
+                  }}
+                  fluid
+                  size="big"
+                >
+                  Dual
+                </Button>
+              </Grid.Column>
+              <Grid.Column>
+                <Button
+                  textAlign="center"
+                  onClick={this.assertSpaceMatch}
+                  fluid
+                  size="big"
+                >
+                  Visual Match
+                </Button>
+              </Grid.Column>
+            </Grid>
+            <Progress
+              color="green"
+              percent={this.state.trialProgress}
+              style={{ marginTop: "25px" }}
+            />
+          </Grid.Column>
+          <Grid.Column
+            width={3}
+            style={{
+              marginTop: "27px",
+              backgroundColor: "#E5E5E5",
+              borderRadius: "3px",
+              padding: "28px",
+            }}
+          >
+            <p>
+              Level:{" "}
+              <span style={{ fontWeight: "bold" }}>{this.state.level}</span>
+            </p>
+            <p>
+              Number of trails:{" "}
+              <span style={{ fontWeight: "bold" }}>{this.state.trials}</span>
+            </p>
             <Dropdown
               simple
               placeholder="N Back"
@@ -244,115 +461,6 @@ class App extends Component {
             />
           </Grid.Column>
         </Grid>
-        <Grid
-          id="grid"
-          style={{ width: "600px", margin: "0 auto", marginTop: "30px" }}
-          columns="equal"
-          celled
-        >
-          <Grid.Row style={{ height: "175px" }}>
-            <Grid.Column>
-              <div
-                style={{ height: "100%" }}
-                className={this.state[0] ? "visible-space" : null}
-                key={0}
-              ></div>
-            </Grid.Column>
-            <Grid.Column>
-              <div
-                style={{ height: "100%" }}
-                className={this.state[1] ? "visible-space" : null}
-                key={1}
-              ></div>
-            </Grid.Column>
-            <Grid.Column>
-              <div
-                style={{ height: "100%" }}
-                className={this.state[2] ? "visible-space" : null}
-                key={2}
-              ></div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row style={{ height: "175px" }}>
-            <Grid.Column>
-              <div
-                style={{ height: "100%" }}
-                className={this.state[3] ? "visible-space" : null}
-                key={3}
-              ></div>
-            </Grid.Column>
-            <Grid.Column>
-              <div
-                style={{ height: "100%" }}
-                className={this.state[4] ? "visible-space" : null}
-                key={4}
-              ></div>
-            </Grid.Column>
-            <Grid.Column>
-              <div
-                style={{ height: "100%" }}
-                className={this.state[5] ? "visible-space" : null}
-                key={5}
-              ></div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row style={{ height: "175px" }}>
-            <Grid.Column>
-              <div
-                style={{ height: "100%" }}
-                className={this.state[6] ? "visible-space" : null}
-                key={6}
-              ></div>
-            </Grid.Column>
-            <Grid.Column>
-              <div
-                style={{ height: "100%" }}
-                className={this.state[7] ? "visible-space" : null}
-                key={7}
-              ></div>
-            </Grid.Column>
-            <Grid.Column>
-              <div
-                style={{ height: "100%" }}
-                className={this.state[8] ? "visible-space" : null}
-                key={8}
-              ></div>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-        <div style={{ width: "600px", margin: "0 auto", marginTop: "30px" }}>
-          <Grid columns={3}>
-            <Grid.Column>
-              <Button onClick={this.assertSoundMatch} fluid size="huge">
-                Audio Match
-              </Button>
-            </Grid.Column>
-            <Grid.Column>
-              <Button
-                onClick={() => {
-                  this.assertSoundMatch()
-                  this.assertSpaceMatch()
-                }}
-                fluid
-                size="huge"
-              >
-                Dual
-              </Button>
-            </Grid.Column>
-            <Grid.Column>
-              <Button onClick={this.assertSpaceMatch} fluid size="huge">
-                Space Match
-              </Button>
-            </Grid.Column>
-          </Grid>
-        </div>
-        <div style={{ width: "650px", margin: "0 auto", marginTop: "30px" }}>
-          <Progress
-            color="green"
-            percent={this.state.trialProgress}
-            style={{ marginTop: "25px" }}
-          />
-        </div>
       </Container>
     )
   }
